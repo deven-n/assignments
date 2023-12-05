71dd90e6eef49592c553ebbdf5ec63d1ec96f208
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-12-05 21:23:13 +0530

    Update findLargestElement.js

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,27 @@
 */
 
 function findLargestElement(numbers) {
-    
-}
+    let largestNumber = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] > largestNumber) {
+        largestNumber = numbers[i];
+      }
+    }
+    return largestNumber;
+  }
+  
+  module.exports = findLargestElement;
 
-module.exports = findLargestElement;
+  // all test cases passed
+
+  /* 
+    PASS  tests/expenditure-analysis.test.js
+    PASS  tests/anagram.test.js
+    PASS  tests/findLargestElement.test.js                                                                              
+
+    Test Suites: 3 passed, 3 total
+    Tests:       13 passed, 13 total
+    Snapshots:   0 total
+    Time:        2.131 s
+    Ran all test suites.
+  */
