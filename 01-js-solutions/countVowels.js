b7302ef146aeac0e19929303c5337821d02bb6fd
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-12-06 10:32:05 +0530

    cleared all test cases for coutVowels

---
+++ 01-js-solutions/countVowels.js
@@ -0,0 +1,26 @@
+/*
+  Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
+  Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
+
+  Once you've implemented the logic, test your code by running
+*/
+
+function countVowels(str) {
+    // Your code here
+
+    let vowels = ["a", "e", "i", "o", "u"];
+    str = str.toLowerCase();
+    let result = 0;
+    for (let vowel of vowels) {
+        for (let i = 0; i < str.length; i++) {
+        if (str[i] === vowel) result++;
+        }
+    }
+
+    return result;
+
+}
+
+module.exports = countVowels; 
+
+// all test cases passed. 
